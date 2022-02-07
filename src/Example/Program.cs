f142fcb74a78d5379e4cc8c4672794926131a760
MikiraSora <mikirasora0409@126.com>
2022-02-08 00:32:14 +0800

    add example project

---
+++ src/Example/Program.cs
@@ -0,0 +1,16 @@
+﻿using SimpleSvg2LineSegementInterpolater;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var lineSegments = await Interpolater.GenerateInterpolatedLineSegmentAsync(await File.ReadAllTextAsync(@"I:\zz.svg"));
+            using var image = Drawing.DrawToImage(lineSegments);
+            Drawing.SaveImageToFile(@"I:\zz.png", image);
+        }
+    }
+}
